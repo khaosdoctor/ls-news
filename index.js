9528a297e0084d4ebc650a1677e039129c4fe046
Lucas Santos <lhs.santoss@gmail.com>
2022-04-30 20:41:09 +0200

    feat: add signup
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 window.onload = function () {
   document.querySelector('footer>span>strong').innerText = `© ${new Date().getFullYear()}`
 }
+
+async function sendMagicLink (e) {
+  const emailInput = document.querySelector('#input-submit-email')
+  const titleElement = document.querySelector('.main-text')
+  const formElement = document.querySelector('.form')
+  const buttonElement = document.querySelector('#button-submit')
+  emailInput.setAttribute('disabled', true)
+  buttonElement.setAttribute('disabled', true)
+  buttonElement.innerText = 'Enviando...'
+
+  const email = emailInput.value
+  const url = 'https://blog.lsantos.dev/members/api/send-magic-link'
+  const payload = {
+    email,
+    name: email,
+    requestSrc: 'landing-page'
+  }
+
+  if (window.fetch) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+
+
+    if (response.status === 201) {
+      formElement.setAttribute('style', 'display: none')
+      titleElement.innerHTML = 'Quase lá! Acesse o seu e-mail para confirmar sua incrição.<p class="bold green-bg">Muito obrigado por se inscrever!</p>'
+      return
+    }
+  }
+}
